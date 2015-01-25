5c04d9acd9dc4a0083344c029c32e7c1915cc4e6
Marc Khristian Partoriza <mkap@hawaii.edu>
2015-01-25 01:00:40 -1000

    Create debugMode.h

---
+++ debugMode.h
@@ -0,0 +1,20 @@
+/*****************************************************************
+ *
+ *  NAME:               Marc Khristian Partoriza
+ *
+ *  HOMEWORK:           Project 2
+ *
+ *  CLASS:              ICS 212
+ *
+ *  INSTRUCTOR:         Ravi Narayan
+ *
+ *  DATE:               Nov 23, 2014
+ *
+ *  FILE:               debugMode.h
+ *
+ *  DESCRIPTION:        This file contains the global variable
+ *                      definition, debugMode.
+ ****************************************************************/
+
+
+extern int debugMode;
